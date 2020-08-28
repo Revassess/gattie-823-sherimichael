0a3bbb6dff472e7e1ed36fe3f8cd0fe1bbde5c5e
Sherilyn Michael <sherimichael@gmail.com>
2020-08-28 18:21:25 -0400

    can't update connectin endpoint

--- Revassess/src/main/java/com/revature/config/ConnectionUtil.java
+++ Revassess/src/main/java/com/revature/config/ConnectionUtil.java
@@ -18,7 +18,7 @@ public class ConnectionUtil {
 	private static ConnectionUtil cu;
 	
 	// add your jdbc url
-	public static final String URL = "jdbc:postgresql://javafs200803.cicoda1qunaq.us-east-2.rds.amazonaws.com:5432/postgres";;
+	public static final String URL = "jdbc:postgresql://javafs200803.cicoda1qunaq.us-east-2.rds.amazonaws.com:5432/tier3";;
 	// add your jdbc username
 	public static final String USERNAME = "postgres";
 	// add your jdbc password
